717a953558ef282532068e167d67392e498f7f69
LimTae-git <56114095+LimTae-git@users.noreply.github.com>
2019-10-08 16:13:08 +0900

    Add files via upload

---
+++ ex_12.php
@@ -0,0 +1,6 @@
+<?php
+$grades = array('egoing' => 10, 'k8805' => 6, 'sorialgi' => 8);
+foreach($grades as $key => $value){
+  echo "key : {$key} value: {$value}<br />";
+}
+ ?>
---
+++ ex_5.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    echo $_GET['id'].','.$_GET['password'];
+     ?>
+
+  </body>
+</html>
